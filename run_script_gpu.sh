6aa639dddac2c786be5386fc448a8220a1aca49e
trungnl2000 <trungnl2000@gmail.com>
2024-05-18 23:52:11 +0200

    fix configurations để avg chạy được

--- run_script_gpu.sh
+++ run_script_gpu.sh
@@ -1,4 +1,4 @@
-# bash scripts_gpu/latency_test_conv_avg.sh
+bash scripts_gpu/latency_test_conv_avg.sh
 bash scripts_gpu/latency_test_conv_hosvd.sh
 bash scripts_gpu/latency_test_conv_svd.sh
 bash scripts_gpu/latency_test_conv.sh
