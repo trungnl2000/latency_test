0e43876e2a6aebb4696b3c1dbd8864c27d222b8c
trungnl2000 <trungnl2000@gmail.com>
2024-05-19 10:58:49 +0200

    đổi toàn bộ config

--- scripts_cpu/latency_test_conv_hosvd.sh
+++ scripts_cpu/latency_test_conv_hosvd.sh
@@ -1,6 +1,6 @@
-python latency_test_conv_hosvd.py --device cpu --in_channels 128 --out_channels 128 --width 120 --height 160 --batch_size 16 --kernel_size 2
-python latency_test_conv_hosvd.py --device cpu --in_channels 256 --out_channels 256 --width 60 --height 80 --batch_size 16 --kernel_size 2
-python latency_test_conv_hosvd.py --device cpu --in_channels 512 --out_channels 512 --width 30 --height 40 --batch_size 16 --kernel_size 2
+python latency_test_conv_hosvd.py --device cpu --in_channels 64 --out_channels 64 --width 60 --height 80 --batch_size 16 --kernel_size 2
+python latency_test_conv_hosvd.py --device cpu --in_channels 128 --out_channels 128 --width 30 --height 40 --batch_size 16 --kernel_size 2
 python latency_test_conv_hosvd.py --device cpu --in_channels 256 --out_channels 256 --width 14 --height 14 --batch_size 16 --kernel_size 2
-python latency_test_conv_hosvd.py --device cpu --in_channels 128 --out_channels 128 --width 28 --height 28 --batch_size 16 --kernel_size 2
-python latency_test_conv_hosvd.py --device cpu --in_channels 64 --out_channels 64 --width 56 --height 56 --batch_size 16 --kernel_size 2
+python latency_test_conv_hosvd.py --device cpu --in_channels 128 --out_channels 128 --width 7 --height 7 --batch_size 16 --kernel_size 2
+python latency_test_conv_hosvd.py --device cpu --in_channels 64 --out_channels 64 --width 14 --height 14 --batch_size 16 --kernel_size 2
+python latency_test_conv_hosvd.py --device cpu --in_channels 32 --out_channels 32 --width 28 --height 28 --batch_size 16 --kernel_size 2
