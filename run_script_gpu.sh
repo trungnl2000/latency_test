ae1c7615f7db0bed51254a6b9f1c9f233b4ac8db
trungnl2000 <trungnl2000@gmail.com>
2024-05-21 13:08:51 +0200

    fix script

--- run_script_gpu.sh
+++ run_script_gpu.sh
@@ -1,4 +1,6 @@
-bash scripts_gpu/latency_test_conv_avg.sh
-bash scripts_gpu/latency_test_conv_hosvd.sh
-bash scripts_gpu/latency_test_conv_svd.sh
-bash scripts_gpu/latency_test_conv.sh
+# bash scripts_gpu/latency_test_conv.sh
+# bash scripts_gpu/latency_test_conv_avg.sh
+# bash scripts_gpu/latency_test_conv_hosvd.sh
+# bash scripts_gpu/latency_test_conv_svd.sh
+bash scripts_gpu/latency_test_conv_hosvd_one_component.sh
+bash scripts_gpu/latency_test_conv_svd_one_component.sh
